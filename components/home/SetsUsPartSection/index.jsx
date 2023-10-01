86c28be6a6cf24b224a2862f1aed44c0d76fe93f
kj2kmusicman <mmm066550@gmail.com>
2023-10-01 12:55:06 +0300

    1/10/2023 12:55PM

---
+++ components/home/SetsUsPartSection/index.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import styles from "./.module.scss";
+import Image from "next/image";
+
+const items = [
+  {
+    icon: "stethoscope",
+    head: "الأستشاريين والأخصائيين",
+    text: "نخبة من الأطباء الأستشاريين والأخصائيين المعتمدين والمؤهلين فى المجال .",
+  },
+  {
+    icon: "drop",
+    head: "وحدة العلاج المائي",
+    text: "وحدة علاج مائي مجهزة بأحدث الوسائل للتعامل مع الحالات الخاصة من ذوي الهمم .",
+  },
+  {
+    icon: "spine",
+    head: "وحدة العمود الفقري",
+    text: "وحدة عمود فقري لعلاج تشوهات الأطفال من ذوي الهمم",
+  },
+  {
+    icon: "disable",
+    head: "ذوي الهمم",
+    text: "يعتبر مركز النسور لذوي الهمم وحدة متكاملة لجميع الحالات من ذوي الهمم .",
+  },
+  {
+    icon: "ambulance",
+    head: "وسائل النقل",
+    text: "توفر وسائل نقل و مواصلات مجهزة وآمنة لنقل الحالات من والى المركز دون عناء .",
+  },
+  {
+    icon: "iso",
+    head: "ISO 9001",
+    text: `اعتماد المركز والأكاديمية من الهيئة الأسترالية للاعتماد والجودة على شهاده الايزو 9001 فى 2015`,
+  },
+];
+
+export default function SetsUsPartSection() {
+  return (
+    <section className={styles.sets__us__up__section}>
+      <div className={styles.section__container}>
+        <div className="container">
+          <div className="row align-items-center justify-content-center">
+            <div className="col-12 col-xl-6 text-center text-xl-end">
+              <div className={styles.text}>
+                <h5>ما يـمـيـزنــا</h5>
+                <p>
+                  ينفرد مركز النسور لذوي الهمم ( زايد الطبي ) بالعديد من
+                  المميزات التي تجعله منفرداً على مستوى مصر وأفريقيا من خلال
+                  الأتي :
+                </p>
+              </div>
+            </div>
+            {items.map((item, index) => {
+              return (
+                <div
+                  key={index}
+                  className="col-12 col-md-6 col-lg-4 col-xl-3 p-4"
+                >
+                  <div className={styles.item__card}>
+                    <Image
+                      width={75}
+                      height={75}
+                      src={`/assets/icons/${item.icon}.svg`}
+                      alt={item.icon}
+                    ></Image>
+                    <h6 className={styles.head}>{item.head}</h6>
+                    <p className={styles.text}>{item.text}</p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@
 
 import HeroSlider from "../../components/home/HeroSlider";
 import CenterVision from "../../components/home/CenterVisionSection";
+import SetsUsPartSection from "../../components/home/SetsUsPartSection";
 
 export default function page() {
   return (
     <main>
       <HeroSlider />
       <CenterVision />
+      <SetsUsPartSection />
     </main>
   );
 }
